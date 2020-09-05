05ecf0ac068030214b9db4dc4d9c82f8a254db74
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-09-04 22:15:22 -0300

    added new components

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 import Onboard from "./pages/onboard.vue";
 import Start from "./pages/start.vue";
+import Translating from "./pages/translating.vue";
 import Vue from "vue";
 
 const routes = {
   "/": Onboard,
   start: Start,
+  translating: Translating,
 };
 
 const APP = new Vue({
@@ -13,7 +15,7 @@ const APP = new Vue({
     view: "",
     inputDir: "",
     outputDir: "",
-    tokenAPI: localStorage.getItem("keyAPI") || "",
+    keyAPI: localStorage.getItem("keyAPI") || "",
   },
   computed: {
     ViewComponent() {
