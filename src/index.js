135e8a737326c216d56de740be8f95a748a743ca
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-10-04 11:43:48 -0300

    global library parameters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const path = require("path");
+const { exit } = require("process");
 
-const inputPath = path.resolve(__dirname, "..", "public", "data");
-const outputPath = path.resolve(__dirname, "..", "public", "dist");
+// const inputPath = path.resolve(__dirname, "..", "public", "data");
+// const outputPath = path.resolve(__dirname, "..", "public", "dist");
+const inputPath = path.resolve("");
+const outputPath = path.resolve("");
 const {core} = require("./core");
 
 (async () => {
